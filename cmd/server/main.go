191a5711da964079756be14ac6c978066c0f6fb2
sumingshan <sumingshan1@flkj.ai>
2023-05-08 18:45:05 +0800

    feat: code optimization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,30 +23,31 @@ func (w *WrapWriter) Write(p []byte) (n int, err error) {
 
 func main() {
 	portForwards := map[string]string{
-		// "material": "-n qa3-elan port-forward svc/material-pkg-common-service 9001:9000",
-		// "archive":  "-n qa3-elan port-forward svc/archive-common-service 9002:9000",
-		// "scm":      "-n qa3-elan port-forward svc/scm-backend-service 9003:9000",
-		// "order":    "-n qa3-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
-		"material": "-n dev1-elan port-forward svc/material-pkg-common-service 9001:9000",
-		"archive":  "-n dev1-elan port-forward svc/archive-common-service 9002:9000",
-		"scm":      "-n dev1-elan port-forward svc/scm-backend-service 9003:9000",
-		"order":    "-n dev1-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
+		"material": "-n qa3-elan port-forward svc/material-pkg-common-service 9002:9000",
+		"archive":  "-n qa3-elan port-forward svc/archive-common-service 9001:9000",
+		"scm":      "-n qa3-elan port-forward svc/scm-backend-service 9003:9000",
+		"order":    "-n qa3-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
+		//"material": "-n dev1-elan port-forward svc/material-pkg-common-service 9001:9000",
+		//"archive":  "-n dev1-elan port-forward svc/archive-common-service 9002:9000",
+		//"scm":      "-n dev1-elan port-forward svc/scm-backend-service 9003:9000",
+		//"order":    "-n dev1-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
 	}
 	g := errgroup.Group{}
 	for k, v := range portForwards {
+		name, cmd := k, v
 		g.Go(func() error {
 			for {
-				fmt.Printf("%s start port forwarding \n", k)
-				p := exec.Command("kubectl", strings.Split(v, " ")...)
-				p.Stdout = &WrapWriter{Writer: os.Stdout, Name: k}
-				p.Stderr = &WrapWriter{Writer: os.Stderr, Name: k}
+				fmt.Printf("%s start port forwarding \n", name)
+				p := exec.Command("/usr/bin/kubectl", strings.Split(cmd, " ")...)
+				p.Stdout = &WrapWriter{Writer: os.Stdout, Name: name}
+				p.Stderr = &WrapWriter{Writer: os.Stderr, Name: name}
 				if err := p.Start(); err != nil {
-					fmt.Printf("%s start prot forwarding failed error:%v \n", k, err)
+					fmt.Printf("%s start prot forwarding failed error:%v \n", name, err)
 					return err
 				}
 				err := p.Wait()
-				fmt.Printf("%s prot forwarding exited error:%v \n", k, err)
-				time.Sleep(time.Second * 10)
+				fmt.Printf("%s prot forwarding exited error:%v \n", name, err)
+				time.Sleep(time.Second * 30)
 			}
 		})
 	}
