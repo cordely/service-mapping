4e340e464995b0f39fe8602b3460e6502b868c33
sumingshan <sumingshan1@flkj.ai>
2023-05-09 12:03:22 +0800

    feat:code finish

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,38 +16,34 @@ type WrapWriter struct {
 }
 
 func (w *WrapWriter) Write(p []byte) (n int, err error) {
-	w.Writer.Write([]byte(w.Name + ": "))
-	fmt.Println()
-	return w.Writer.Write(p)
+	return w.Writer.Write(append([]byte(fmt.Sprintf("%v:\n", w.Name)), p...))
 }
 
 func main() {
 	portForwards := map[string]string{
-		"material": "-n qa3-elan port-forward svc/material-pkg-common-service 9002:9000",
 		"archive":  "-n qa3-elan port-forward svc/archive-common-service 9001:9000",
+		"material": "-n qa3-elan port-forward svc/material-pkg-common-service3 9002:9000",
 		"scm":      "-n qa3-elan port-forward svc/scm-backend-service 9003:9000",
 		"order":    "-n qa3-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
-		//"material": "-n dev1-elan port-forward svc/material-pkg-common-service 9001:9000",
-		//"archive":  "-n dev1-elan port-forward svc/archive-common-service 9002:9000",
+		//"archive":  "-n dev1-elan port-forward svc/archive-common-service 9001:9000",
+		//"material": "-n dev1-elan port-forward svc/material-pkg-common-service 9002:9000",
 		//"scm":      "-n dev1-elan port-forward svc/scm-backend-service 9003:9000",
 		//"order":    "-n dev1-ssmrt port-forward svc/ssc-order-common-service 9004:9000",
 	}
 	g := errgroup.Group{}
 	for k, v := range portForwards {
-		name, cmd := k, v
+		name, args := k, v
 		g.Go(func() error {
 			for {
-				fmt.Printf("%s start port forwarding \n", name)
-				p := exec.Command("/usr/bin/kubectl", strings.Split(cmd, " ")...)
-				p.Stdout = &WrapWriter{Writer: os.Stdout, Name: name}
-				p.Stderr = &WrapWriter{Writer: os.Stderr, Name: name}
-				if err := p.Start(); err != nil {
+				cmd := exec.Command("/usr/bin/kubectl", strings.Split(args, " ")...)
+				cmd.Stdout = &WrapWriter{Writer: os.Stdout, Name: name}
+				cmd.Stderr = &WrapWriter{Writer: os.Stderr, Name: name}
+				if err := cmd.Start(); err != nil {
 					fmt.Printf("%s start prot forwarding failed error:%v \n", name, err)
 					return err
 				}
-				err := p.Wait()
-				fmt.Printf("%s prot forwarding exited error:%v \n", name, err)
-				time.Sleep(time.Second * 30)
+				_ = cmd.Wait()
+				time.Sleep(time.Second * 10)
 			}
 		})
 	}
